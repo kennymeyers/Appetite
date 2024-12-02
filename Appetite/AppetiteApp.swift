34679a0f63458ad1a3e259f4a597d3b1cf061f54
Kenny Meyers <k@kenny.dev>
2024-12-02 11:05:57 -0800

    Initial Commit

---
+++ Appetite/AppetiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetiteApp.swift
+//  Appetite
+//
+//  Created by Kenny Meyers on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
